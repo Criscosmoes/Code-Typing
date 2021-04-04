ccc63af900a1f0c28995b589b3a0da67c0cab77c
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-04-04 10:57:34 -0700

    changed up index.js file

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}, () => console.log("Connected to DB")); 
 
+
+
+
+if (process.env.NODE_ENV === "production"){
+    server.use(express.static("client/build")); 
+
+
+
+    const path = reqiure("path"); 
+    server.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); 
+    })
+}
+
+
+
 const PORT = process.env.PORT || 4000; 
 
 
