f4c38b767bd8e242dfc17a2768a139dbbad7abfd
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-04-04 19:32:05 -0700

    test deployment

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const server = require("./api/server");
 dotenv.config(); 
 
 
-//Connect to DB
-
-mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}, () => console.log("Connected to DB")); 
-
-
-
-
 if (process.env.NODE_ENV === "production"){
 
     console.log("this is a test"); 
     const path = require("path"); 
 
-    server.use(express.static(path.join(__dirname, '/client/build')));
+    server.use(express.static(path.join(__dirname, 'client/build')));
 
     server.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); 
+        console.log("this worked"); 
     })
 }
+
+
+//Connect to DB
+
+mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}, () => console.log("Connected to DB")); 
+
 
 
 
