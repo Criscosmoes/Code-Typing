4fb213f9ae9465755e5a52ed3a0aef69ed30cae8
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2021-04-04 22:23:08 -0700

    testing

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ dotenv.config();
 
 if (process.env.NODE_ENV === "production"){
 
-
-    server.use(express.static("client/build")); 
-
-
     const path = require("path"); 
+
+
+    server.use(express.static(path.join(__dirname, 'client/build')));
+
     server.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); 
     })
